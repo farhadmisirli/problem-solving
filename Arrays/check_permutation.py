39022d59190e740c030a656f3c7a2d5fafb7f1ef
Farhad Misirli <farhadmisirli@Farhads-MacBook-Air.local>
2023-03-30 19:20:40 +0400

    Arrays: check_permuation new solution added using characters' count

--- Arrays/check_permutation.py
+++ Arrays/check_permutation.py
@@ -18,6 +18,34 @@ def solution_using_sorting(a: str, b: str) -> bool:
     return sorted_a == sorted_b
 
 
+def solution_using_characters_count(a: str, b: str) -> bool:
+    # return False if the length of strings is different
+    if len(a) < len(b):
+        return False
+
+    char_count_dict = {}
+
+    for c in a:
+        if c in char_count_dict:
+            char_count_dict[c] += 1
+        else:
+            char_count_dict[c] = 1
+
+    for c in b:
+        if c in char_count_dict:
+            char_count_dict[c] -= 1
+        else:
+            return False
+
+    # check char_count_dict. if char counts are same, value of your count_dic will be 0
+    for key, count in char_count_dict.items():
+        if count != 0:
+            return False
+
+    return True
+
+
 a = "abc"
 b = "bac"
 print(solution_using_sorting(a, b))
+print(solution_using_characters_count(a, b))
